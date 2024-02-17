00c6d64d629afe6f9aeaa845d21850695ba94fc6
Yogesh <yogesh.jaguri2401@gmail.com>
2024-02-17 12:05:52 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,27 @@
+// require the library
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// retrieve MongoDB connection string from environment variable
+const mongodbUrl = process.env.MONGODB_URL;
+
+// connect to the database
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+});
+
+// acquire the connection (to check if it's successful)
+const db = mongoose.connection;
+
+// handle error
+db.on("error", function (err) {
+  console.error("MongoDB connection error:", err.message);
+});
+
+// up and running, then print the message
+db.once("open", function () {
+  console.log("Successfully connected to the MongoDB database");
+});
+
+// export the connection
+module.exports = db;
